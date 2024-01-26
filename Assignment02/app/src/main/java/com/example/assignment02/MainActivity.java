4c4e30e33eff9785710936e63705a44db610537d
Nicholas Boyle <nboyle@uncc.edu>
2024-01-26 16:53:41 -0500

    finished layout for Assignment 02

---
+++ Assignment02/app/src/main/java/com/example/assignment02/MainActivity.java
@@ -0,0 +1,22 @@
+/*
+    Assignment 02
+
+    MainActivity.java
+
+    Group 1: Nicholas Boyle, Shawntraree Lucas
+ */
+
+package com.example.assignment02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
